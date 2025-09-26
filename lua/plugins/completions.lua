4f500c1da11b1e204ed6dda846554679e6a73d2a
g-sierra <193622299+g-sierra@users.noreply.github.com>
2025-09-26 16:41:28 -0300

    Change nvim-cmp to blink.cmp

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -1,51 +1,15 @@
 return {
-    {
-        "hrsh7th/cmp-nvim-lsp",
-    },
-    {
-        "hrsh7th/cmp-nvim-lsp-signature-help",
-    },
-    {
-        "L3MON4D3/LuaSnip",
-        dependencies = {
-            "saadparwaiz1/cmp_luasnip",
-            "rafamadriz/friendly-snippets",
-        },
-    },
-    {
-        "hrsh7th/nvim-cmp",
-        config = function()
-            local cmp = require("cmp")
-            require("luasnip.loaders.from_vscode").lazy_load()
-
-            cmp.setup({
-                snippet = {
-                    expand = function(args)
-                        require("luasnip").lsp_expand(args.body)
-                    end,
-                },
-                window = {
-                    completion = cmp.config.window.bordered(),
-                    documentation = cmp.config.window.bordered(),
-                },
-                mapping = cmp.mapping.preset.insert({
-                    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-                    ["<C-f>"] = cmp.mapping.scroll_docs(4),
-                    ["<C-Space>"] = cmp.mapping.complete(),
-                    ["<C-e>"] = cmp.mapping.abort(),
-                    ["<Tab>"] = cmp.mapping.confirm({ select = true }),
-                }),
-                sources = cmp.config.sources({
-                    { name = 'nvim_lsp' },
-                    { name = 'nvim_lsp_signature_help' },
-                    { name = "luasnip" },
-                }, {
-                        { name = "buffer" },
-                    }),
-                experimental = {
-                    ghost_text = true,
-                }
-            })
-        end,
-    },
+	{
+		"saghen/blink.cmp",
+		dependencies = { "rafamadriz/friendly-snippets" },
+		version = "1.*",
+		opts = {
+			keymap = { preset = "super-tab" },
+			appearance = {
+				nerd_font_variant = "mono",
+			},
+			signature = { enabled = true },
+			fuzzy = { implementation = "prefer_rust_with_warning" },
+		},
+	},
 }
--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -15,8 +15,9 @@ return {
 	},
 	{
 		"neovim/nvim-lspconfig",
+		dependencies = { "sahgen/blink.cmp" },
 		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			local capabilities = require("blink.cmp").get_lsp_capabilities()
 			vim.lsp.config("*", {
 				capabilities = { capabilities },
 				settings = {

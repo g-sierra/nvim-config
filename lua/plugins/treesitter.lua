2557ebf82a8eee2516ca78de116ab8354ebb8a07
g-sierra <193622299+g-sierra@users.noreply.github.com>
2025-09-26 01:37:18 -0300

    Add linked tag editing in treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,15 +1,16 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    branch = "master",
-    lazy = false,
-    build = ":TSUpdate",
-    config = function()
-        local config = require("nvim-treesitter.configs")
-        config.setup({
-            ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
-            auto_install = true,
-            highlight = { enable = true },
-            indent = { enable = true },
-        })
-    end,
+	"nvim-treesitter/nvim-treesitter",
+	branch = "master",
+	lazy = false,
+	build = ":TSUpdate",
+	config = function()
+		local config = require("nvim-treesitter.configs")
+		config.setup({
+			ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
+			auto_install = true,
+			highlight = { enable = true },
+			indent = { enable = true },
+			auto_tag = { enable = true },
+		})
+	end,
 }

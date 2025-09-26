4fa06a499b502133059f41e5f41b43660a284ba7
g-sierra <193622299+g-sierra@users.noreply.github.com>
2025-09-26 16:08:23 -0300

    Change method of handling vim global variable

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -19,13 +19,20 @@ return {
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 			vim.lsp.config("*", {
 				capabilities = { capabilities },
+				settings = {
+					Lua = {
+						diagnostics = {
+							globals = { "vim" },
+						},
+					},
+				},
 			})
 			-- vim.lsp.enable({ "lua_ls", "ts_ls" })  -- this is called automatically by mason-lspconfig.nvim
 
 			vim.diagnostic.config({ virtual_text = true })
 
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-            vim.keymap.set("n", "gh", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gh", vim.lsp.buf.hover, {})
 			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
 		end,
 	},

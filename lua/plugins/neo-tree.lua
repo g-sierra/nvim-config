901ac06be86f4c95db598cd29f99d0e52d1abb66
g-sierra <193622299+g-sierra@users.noreply.github.com>
2025-09-26 00:28:42 -0300

    Update neo-tree config: show hidden files by default

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,26 +1,31 @@
 return {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-        "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-        "MunifTanjim/nui.nvim",
-    },
-    lazy = false, -- neo-tree will lazily load itself
-    config = function()
-        require("neo-tree").setup({
-            event_handlers = {
-                {
-                    event = "file_open_requested",
-                    handler = function()
-                        -- auto close
-                        -- vim.cmd("Neotree close")
-                        -- OR
-                        require("neo-tree.command").execute({ action = "close" })
-                    end,
-                },
-            },
-        })
-        vim.keymap.set("n", "<C-e>", ":Neotree filesystem reveal toggle<CR>", {})
-    end,
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+		"MunifTanjim/nui.nvim",
+	},
+	lazy = false, -- neo-tree will lazily load itself
+	config = function()
+		require("neo-tree").setup({
+			filesystem = {
+				filtered_items = {
+					visible = true,
+				},
+			},
+			event_handlers = {
+				{
+					event = "file_open_requested",
+					handler = function()
+						-- auto close
+						-- vim.cmd("Neotree close")
+						-- OR
+						require("neo-tree.command").execute({ action = "close" })
+					end,
+				},
+			},
+		})
+		vim.keymap.set("n", "<C-e>", ":Neotree filesystem reveal toggle<CR>", {})
+	end,
 }

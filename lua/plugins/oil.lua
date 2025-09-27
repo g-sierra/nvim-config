e4048f3f8aa976c940bf547c694a0df4a7228f38
g-sierra <193622299+g-sierra@users.noreply.github.com>
2025-09-27 20:14:53 -0300

    Add show hidden by default

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -6,6 +6,9 @@ return {
     dependencies = { { "nvim-mini/mini.icons", opts = {} } },
     lazy = false,
     opts = {
+      view_options = {
+        show_hidden = true,
+      },
       win_options = {
         signcolumn = "yes:2",
       },
@@ -16,7 +19,7 @@ return {
         function()
           require("oil").toggle_float()
         end,
-        desc = "open oil in parent directory"
+        desc = "open oil in parent directory",
       },
     },
   },

ff1d4afc48bb2aeadd14b7f5c89d031646f5bb58
g-sierra <193622299+g-sierra@users.noreply.github.com>
2025-09-27 19:19:56 -0300

    Update telescope config

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,12 +4,23 @@ return {
     tag = "0.1.8",
     dependencies = {
       "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope-ui-select.nvim",
       {
         "nvim-telescope/telescope-fzf-native.nvim",
         build = "make",
       },
     },
     config = function()
+      require("telescope").setup({
+        extensions = {
+          ["fzf"] = {},
+          ["ui-select"] = {
+            require("telescope.themes").get_cursor(),
+          },
+        },
+      })
+
+      -- keymaps
       local builtin = require("telescope.builtin")
       vim.keymap.set("n", "<C-p>", builtin.find_files, { desc = "Telescope find files" })
       vim.keymap.set("n", "<leader>ff", builtin.git_files, { desc = "Telescope find git files" })
@@ -22,20 +33,9 @@ return {
           cwd = vim.fn.stdpath("config"),
         })
       end, { desc = "Edit Neovim config" })
-    end,
-  },
-  {
-    "nvim-telescope/telescope-ui-select.nvim",
-    config = function()
-      require("telescope").setup({
-        extensions = {
-          ["ui-select"] = {
-            require("telescope.themes").get_dropdown({
-              -- even more opts
-            }),
-          },
-        },
-      })
+
+      -- load extensions
+      require("telescope").load_extension("fzf")
       require("telescope").load_extension("ui-select")
     end,
   },

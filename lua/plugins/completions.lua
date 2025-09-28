944a58406a8378669080b03416c77db3ae4151f2
g-sierra <193622299+g-sierra@users.noreply.github.com>
2025-09-27 23:11:38 -0300

    Change copilot accept suggestion keymap

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -1,6 +1,13 @@
 return {
   {
     "github/copilot.vim",
+    config = function()
+      vim.keymap.set("i", "<C-y>", 'copilot#Accept("\\<CR>")', {
+        expr = true,
+        replace_keycodes = false,
+      })
+      vim.g.copilot_no_tab_map = true
+    end,
   },
   {
     "saghen/blink.cmp",

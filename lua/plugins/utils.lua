3fba51884be0a3c8df8753907b4d073904bf24a6
g-sierra <193622299+g-sierra@users.noreply.github.com>
2025-09-28 01:27:26 -0300

    Add which-key

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -27,4 +27,20 @@ return {
       })
     end,
   },
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    opts = {
+      preset = "helix",
+    },
+    keys = {
+      {
+        "<leader>wk",
+        function()
+          require("which-key").show({ global = false })
+        end,
+        desc = "Buffer Local Keymaps (which-key)",
+      },
+    },
+  },
 }

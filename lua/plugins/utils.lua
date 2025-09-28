2ef9275a676972bffe0937c5abdda381d307c564
g-sierra <193622299+g-sierra@users.noreply.github.com>
2025-09-28 01:18:40 -0300

    Add indent-blankline

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -10,4 +10,21 @@ return {
     event = "InsertEnter",
     config = true,
   },
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    config = function()
+      vim.api.nvim_set_hl(0, "IblScope", { fg = "#fe8019" })
+
+      require("ibl").setup({
+        indent = {
+          char = "▏",
+        },
+        scope = {
+          enabled = true,
+          highlight = "IblScope",
+        },
+      })
+    end,
+  },
 }
